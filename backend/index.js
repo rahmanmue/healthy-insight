64cedc4f9bafd6c5dedc41429ef14144d2be683c
rahman.muraman <rahman.muraman@gmail.com>
2024-07-17 17:55:36 +0700

    feat: update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,48 @@
 import express from "express";
+import db from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import User from "./models/UserModel.js";
+import Penyakit from "./models/PenyakitModel.js";
+import Gejala from "./models/GejalaModel.js";
+import Solusi from "./models/SolusiModel.js";
+import Revise from "./models/ReviseModel.js";
+import BasisPengetahuan from "./models/BasisPengetahuanModel.js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5001;
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const connectDB = async () => {
+  try {
+    await db.sync();
+    console.log("Database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const main = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
