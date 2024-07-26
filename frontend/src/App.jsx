f8bc2376c2d575fa4d26ed3bc09e65dcd7b068e5
rahman.muraman <rahman.muraman@gmail.com>
2024-07-27 00:06:26 +0700

    feat: update routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,32 +3,36 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Penyakit from "./pages/Penyakit/Penyakit";
-import Case from "./pages/Case/Case";
+import Kasus from "./pages/Kasus/Kasus";
 import Gejala from "./pages/Gejala/Gejala";
-import User from "./pages/User/User";
+import Akun from "./pages/Akun/Akun";
 import BasisPengetahuan from "./pages/BasisPengetahuan/BasisPengetahuan";
 import AddBasisPengetahuan from "./pages/BasisPengetahuan/AddBasisPengetahuan";
 import DetailBasisPengetahuan from "./pages/BasisPengetahuan/DetailBasisPengetahuan";
-import DetailCase from "./pages/Case/DetailCase";
-// import PrivateRoute from "./routes/PrivateRoute";
+import DetailKasus from "./pages/Kasus/DetailKasus";
+import Solusi from "./pages/Solusi/Solusi";
+import EditSolusi from "./pages/Solusi/EditSolusi";
+import Konsultasi from "./pages/Konsultasi/Konsultasi";
 import MainLayout from "./components/MainLayout/MainLayout";
+import AddSolusi from "./pages/Solusi/AddSolusi";
+// import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout>
         <Routes>
-          {/* <Route path="/" element={<PrivateRoute roles={["admin", "user"]} />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route> */}
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/konsultasi" element={<Konsultasi />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/kasus/hasil/:kode_case" element={<DetailKasus />} />
+
+          {/* private route admin */}
           <Route path="/register" element={<Register />} />
-          <Route path="/kasus" element={<Case />} />
+          <Route path="/admin/kasus" element={<Kasus />} />
           <Route path="/admin/penyakit" element={<Penyakit />} />
           <Route path="/admin/gejala" element={<Gejala />} />
-          <Route path="/admin/user" element={<User />} />
           <Route
             path="/admin/basis-pengetahuan"
             element={<BasisPengetahuan />}
@@ -41,8 +45,10 @@ function App() {
             path="/admin/basis-pengetahuan/detail/:kode_bp"
             element={<DetailBasisPengetahuan />}
           />
-
-          <Route path="/case/hasil/:kode_case" element={<DetailCase />} />
+          <Route path="/admin/solusi" element={<Solusi />} />
+          <Route path="/admin/solusi/edit/:id" element={<EditSolusi />} />
+          <Route path="/admin/solusi/add" element={<AddSolusi />} />
+          <Route path="/admin/akun" element={<Akun />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
