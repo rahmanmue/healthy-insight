3b71463ea57c62e090e8a9d30ceaab279c1cb591
rahman.muraman <rahman.muraman@gmail.com>
2024-08-21 05:10:39 +0700

    feat: add theme & config dark mode

--- frontend/src/contexts/SidebarContext.jsx
+++ frontend/src/contexts/SidebarContext.jsx
@@ -1,16 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const SidebarContext = createContext();
 
 const SidebarContextProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true);
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    console.log(theme);
+  };
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <SidebarContext.Provider value={{ isOpen, toggleSidebar }}>
+    <SidebarContext.Provider
+      value={{ isOpen, toggleSidebar, changeTheme, theme }}
+    >
       {children}
     </SidebarContext.Provider>
   );
